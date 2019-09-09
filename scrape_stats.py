bbcd84419ac1e5abb7273b769a0022aee6cabeb2
gumdropsteve <warobson@gmail.com>
2019-09-09 05:11:58 -0700

    upgraded with modifications from modify.ipynb

--- scrape_stats.py
+++ scrape_stats.py
@@ -4,15 +4,19 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 import calendar
+from user import u, p
 
-# Google email
-usernameStr = 'xxx'
-
+# account
+usernameStr = u
 # password
-passwordStr = 'xxx'
+passwordStr = p
 
+# tag the options field
+options = webdriver.FirefoxOptions()  
+# disable push/popups 
+options.set_preference("dom.push.enabled", False)  
 # initialize Selenium webdriver
-browser = webdriver.Chrome()
+browser = webdriver.Firefox(options=options)
 
 # open new tab with link to sign-in options for Medium
 browser.get('https://medium.com/m/signin?redirect=https%3A%2F%2Fmedium.com%2F&operation=login')
@@ -49,10 +53,14 @@ browser.get('https://medium.com/me/stats')
 time.sleep(2)
 
 # number of months to get stats for - adjust as you please
-months = 10
+months = 6
 
-viewstats = []     # number of views
-readstats = []     # number of reads
+# number of views
+viewstats = []
+# number of reads
+readstats = []
+# number of fans
+fanstats = []
 i = 0
 while i < months:
     # default data is on views
@@ -63,10 +71,17 @@ while i < months:
     # line below clicks on the "Reads" tab on the page - graph now shows read history
     browser.find_element_by_xpath("//li[@data-action='switch-graph'][@data-action-value='reads']").click()
     time.sleep(2)
-    
     # parse page, and get data
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     readstats.append(soup.find_all('rect'))
+    
+    # get stats on article fans as well - this will be in reverse order from views
+    # line below clicks on the "Fans" tab on the page - graph now shows read history
+    browser.find_element_by_xpath("//li[@data-action='switch-graph'][@data-action-value='fans']").click()
+    time.sleep(2)
+    # parse page, and get data
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    fanstats.append(soup.find_all('rect'))
     
     # increase counter
     i += 1
@@ -89,21 +104,21 @@ statInfo = []
 for j in range(len(viewstats)):
     # for each day in a selected 30-day period...
     for k in range(len(viewstats[j])):
-        # add the year to the view date - 2019 if January, 2018 otherwise
-        # not robust enough, but does the trick here...
-        # and also get number of views
-        # put both in a list, and append to array
+        # add the year to the view date
         viewDate = viewstats[j][k]['data-tooltip'].split('on')[-1].strip()
         if viewDate.split('\xa0')[0].strip() == 'January':
             viewDate = viewDate + ' 2019'
         else:
-            viewDate = viewDate + ' 2018'
+            viewDate = viewDate + ' 2019'
+        # extract numbers
         numViews = int(viewstats[j][k]['data-tooltip'].split(' ')[0])
         numReads = int(readstats[j][k]['data-tooltip'].split(' ')[0])
-        statInfo.append([viewDate, numViews, numReads])
+        numFans = int(fanstats[j][k]['data-tooltip'].split(' ')[0])
+        # add collected numbers to pool 
+        statInfo.append([viewDate, numViews, numReads, numFans])
         
 # dataframe to store the number of views for the article each day
-statData = pd.DataFrame(statInfo, columns=['date', 'numberOfViews', 'numberOfReads'])
+statData = pd.DataFrame(statInfo, columns=['date', 'numberOfViews', 'numberOfReads', 'numberOfFans'])
 
 # create a string showing the full day and convert to datetime (Python time format)
 statData['newDate'] = statData['date'].map(lambda x: x.split('\xa0')[0].strip() + '\\' + x.split('\xa0')[1].split(' ')[0].strip()+ '\\' + x.split('\xa0')[1].split(' ')[1].strip())
@@ -114,10 +129,10 @@ statData['Weekday'] = statData.newDate.map(lambda x: calendar.day_name[x.weekday
 statData.sort_values(by='newDate', inplace=True)
 
 # rename columns, drop redundant columns, and re-order columns
-statData.columns = ['oldDate', 'Views', 'Reads', 'Date', 'Weekday']
+statData.columns = ['oldDate', 'Views', 'Reads', 'Fans', 'Date', 'Weekday']
 statData.reset_index(inplace=True)
 statData.drop(labels=['oldDate', 'index'], axis=1, inplace=True)
-statData = statData[['Date', 'Weekday', 'Views', 'Reads']]
+statData = statData[['Date', 'Weekday', 'Views', 'Reads', 'Fans']]
 
 # save to Excel if you want
-statData.to_excel(pd.ExcelWriter('medium_stats.xlsx'),'Stats', index=False)
+statData.to_csv('medium_stats.csv', index=False)
